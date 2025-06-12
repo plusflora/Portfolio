3539bf462777caaf7f251fa0d4b6f6d1f352bd84
plusflora <v.bouquet707@gmail.com>
2025-06-12 15:05:30 -0700

    adds intro blurb

---
+++ src/components/IntroBlurb.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// I want this to load in in the center of the screen and then scroll up a little and have the options fade in below it
+
+function IntroBlurb() {
+  return (
+    <div className="intro blurb">
+      <h2>Hi there, I'm</h2>
+      <h1>Vince Bouquet</h1>
+      <p>
+        A full-stack developer blending code with creativity—driven by a passion
+        for art, design, and building beautiful things that work.
+      </p>
+    </div>
+  );
+}
+
+export default IntroBlurb;
